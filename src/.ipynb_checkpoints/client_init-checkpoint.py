a80e694ca99bb953c113429a3212660de1b04457
Burak Gozluklu <gozburak@amazon.com>
2024-06-04 10:47:20 -0400

    Cleaned version

--- src/.ipynb_checkpoints/client_init-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-# Initialize the Bedrock client
-bedrock_client = boto3.client(service_name='bedrock-runtime')
-bedrock_agent_client = boto3.client("bedrock-agent-runtime", region_name = 'us-east-1')
-accept = "application/json"
-contentType = "application/json"
